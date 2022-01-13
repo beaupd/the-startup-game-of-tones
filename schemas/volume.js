33365ae90401ef85e3ae83d3a87ffa1c9afed427
beaupd <beau@patrickdekker.nl>
2022-01-13 14:01:35 +0100

    New schema Volume fully + dark theme

--- schemas/volume.js
+++ schemas/volume.js
@@ -1,24 +1,231 @@
 export default {
-    name: 'volume',
-    title: 'Volume',
-    type: 'document',
-    fields: [
-      {
-        name: 'title',
-        title: 'Side Title',
-        type: 'string',
-        validation: (Rule) => Rule.required(),
-      },
-      {
-        name: 'color',
-        title: 'Color',
-        type: 'string',
-        validation: (Rule) => Rule.required(),
-      },
-      {
-        name: 'content',
-        title: 'Content',
-        type: 'markdown',
-      },
-    ],
-  };
+	name: 'volume',
+	title: 'Volume',
+	type: "document",
+	fields: [
+		{
+			name: "intro",
+            title: "Introduction of Volume",
+			type: 'object',
+			fields: [
+				{
+					name: 'title',
+					title: 'Side Title',
+					type: 'string',
+				},
+				{
+					name: 'color',
+					title: 'Color',
+					type: 'string',
+				},
+				{
+					name: 'content',
+					title: 'Content',
+					type: 'markdown',
+				},
+				{
+					name: "les",
+					title: "Les",
+					type: "object",
+					fields: [
+						{
+							name: 'title',
+							title: 'Side Title',
+							type: 'string',
+						},
+
+					]
+				}
+			],
+		},
+		{
+			name: "chapter",
+			title: "Chapter",
+			type: "object",
+			fields: [
+				{
+					name: "intro",
+            		title: "Introduction of Chapter",
+					type: 'object',
+					fields: [
+						{
+							name: 'title',
+							title: 'Side Title',
+							type: 'string',
+						},
+						{
+							name: 'color',
+							title: 'Color',
+							type: 'string',
+						},
+						{
+							name: 'content',
+							title: 'Content',
+							type: 'markdown',
+						},
+						{
+							name: "les",
+							title: "Les",
+							type: "object",
+							fields: [
+								{
+									name: 'title',
+									title: 'Side Title',
+									type: 'string',
+								},
+
+							]
+						}
+					],
+				},
+				{
+					name: 'subchapter',
+					title: 'Subchapter',
+					type: "object",
+					fields: [
+						{
+							name: "theory",
+							title: "In theory",
+							type: 'object',
+							fields: [
+								{
+									name: 'title',
+									title: 'Side Title',
+									type: 'string',
+								},
+								{
+									name: 'color',
+									title: 'Color',
+									type: 'string',
+								},
+								{
+									name: 'content',
+									title: 'Content',
+									type: 'markdown',
+								},
+								{
+									name: "les",
+									title: "Les",
+									type: "object",
+									fields: [
+										{
+											name: 'title',
+											title: 'Side Title',
+											type: 'string',
+										},
+
+									]
+								}
+							],
+						},
+						{
+							name: "practice",
+							title: "In practice",
+							type: 'object',
+							fields: [
+								{
+									name: 'title',
+									title: 'Side Title',
+									type: 'string',
+								},
+								{
+									name: 'color',
+									title: 'Color',
+									type: 'string',
+								},
+								{
+									name: 'content',
+									title: 'Content',
+									type: 'markdown',
+								},
+								{
+									name: "les",
+									title: "Les",
+									type: "object",
+									fields: [
+										{
+											name: 'title',
+											title: 'Side Title',
+											type: 'string',
+										},
+
+									]
+								}
+							],
+						},
+						{
+							name: "action",
+							title: "In action",
+							type: 'object',
+							fields: [
+								{
+									name: 'title',
+									title: 'Side Title',
+									type: 'string',
+								},
+								{
+									name: 'color',
+									title: 'Color',
+									type: 'string',
+								},
+								{
+									name: 'content',
+									title: 'Content',
+									type: 'markdown',
+								},
+								{
+									name: "les",
+									title: "Les",
+									type: "object",
+									fields: [
+										{
+											name: 'title',
+											title: 'Side Title',
+											type: 'string',
+										},
+
+									]
+								}
+							],
+						},
+						{
+							name: "exercise",
+							title: "In exercise",
+							type: 'object',
+							fields: [
+								{
+									name: 'title',
+									title: 'Side Title',
+									type: 'string',
+								},
+								{
+									name: 'color',
+									title: 'Color',
+									type: 'string',
+								},
+								{
+									name: 'content',
+									title: 'Content',
+									type: 'markdown',
+								},
+								{
+									name: "les",
+									title: "Les",
+									type: "object",
+									fields: [
+										{
+											name: 'title',
+											title: 'Side Title',
+											type: 'string',
+										},
+
+									]
+								}
+							],
+						},
+					]
+				}
+			]
+		}
+	]
+};
