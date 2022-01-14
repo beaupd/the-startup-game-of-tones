86d43778fbef4cdc042aff61a88063d9bacfdfe5
beaupd <beau@patrickdekker.nl>
2022-01-14 12:38:15 +0100

    merge

--- pages/onboarding.js
+++ pages/onboarding.js
@@ -1,63 +1,3 @@
-<<<<<<< HEAD
-import Head from "next/head";
-
-const IndexPage = () => {
-  return (
-    <>
-      <Head>
-        <script src="/script.js" defer></script>
-      </Head>
-      <main className="container">
-        <svg
-          width="60px"
-          height="60px"
-          className="musicNote"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="#8D74B1"
-        >
-          <path
-            fill="#8D74B1"
-            d="M13 4h4v4h-4v9c0 1-.6 3-3 3s-3-2-3-3 .6-3 3-3 3 2 3 3V4z"
-          />
-          <path
-            stroke="#8D74B1"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M13 17V8m0 9c0 1-.6 3-3 3s-3-2-3-3 .6-3 3-3 3 2 3 3zm0-9V4h4v4h-4z"
-          />
-        </svg>
-
-        <div className="background-image" id="bgImage"></div>
-
-        <h1>The Game of Tones</h1>
-
-        <p className="para">Creative Music making</p>
-
-        <h2 id="uspText">
-          <span>Aqcuire</span> the
-          <span> tools</span> to
-          <span> discover</span>
-          <br /> and <span>express</span> your
-          <span> own musical style</span>
-        </h2>
-
-        <div className="radioButtons">
-          <input id="usp1" type="radio" name="usp" defaultChecked />
-          <input id="usp2" type="radio" name="usp" />
-          <input id="usp3" type="radio" name="usp" />
-        </div>
-        <a id="button" href="/home.html">
-          SKIP
-        </a>
-      </main>
-    </>
-  );
-};
-
-export default IndexPage;
-=======
 import Head from "next/head"
 import styles from "../styles/IndexPage.module.css"
 
@@ -99,4 +39,3 @@ const IndexPage = () => {
 }
 
 export default IndexPage
->>>>>>> ad5a54378d6a48d09ac651cc7a76406a7d77d44d
