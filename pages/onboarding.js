9b8f1219a9618acb5517a5f0b246b3edc8599f9e
Finnvb <finnvb@gmail.com>
2022-01-14 12:26:54 +0100

    Styling op modulair niveau

--- pages/onboarding.js
+++ pages/onboarding.js
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import styles from "../styles/IndexPage.module.css"
 
 
 const IndexPage = () => {
@@ -8,15 +9,15 @@ const IndexPage = () => {
             <Head>
                 <script src="/script.js" defer></script>
             </Head>
-            <main className="container">
+            <main className={styles.container}>
 
-                <svg width="60px" height="60px" className="musicNote" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#8D74B1"><path fill="#8D74B1" d="M13 4h4v4h-4v9c0 1-.6 3-3 3s-3-2-3-3 .6-3 3-3 3 2 3 3V4z" /><path stroke="#8D74B1" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 17V8m0 9c0 1-.6 3-3 3s-3-2-3-3 .6-3 3-3 3 2 3 3zm0-9V4h4v4h-4z" /></svg>
+                <svg width="60px" height="60px" className={styles.musicNote} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#8D74B1"><path fill="#8D74B1" d="M13 4h4v4h-4v9c0 1-.6 3-3 3s-3-2-3-3 .6-3 3-3 3 2 3 3V4z" /><path stroke="#8D74B1" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 17V8m0 9c0 1-.6 3-3 3s-3-2-3-3 .6-3 3-3 3 2 3 3zm0-9V4h4v4h-4z" /></svg>
 
-                <div className="background-image" id="bgImage"></div>
+                <div className={styles.background_image} id="bgImage"></div>
 
                 <h1>The Game of Tones</h1>
 
-                <p className="para">Creative Music making</p>
+                <p className={styles.para}>Creative Music making</p>
 
                 <h2 id="uspText">
                     <span>Aqcuire</span> the 
@@ -26,12 +27,12 @@ const IndexPage = () => {
                     <span> own musical style</span>
                 </h2>
 
-                <div className="radioButtons">
+                <div className={styles.radioButtons}>
                     <input id="usp1" type="radio" name="usp" defaultChecked />
                     <input id="usp2" type="radio" name="usp" />
                     <input id="usp3" type="radio" name="usp" />
                 </div>
-                <a id="button" href="/home.html">SKIP</a>
+                <a id="button" href="/login">SKIP</a>
             </main>
         </>
     )
